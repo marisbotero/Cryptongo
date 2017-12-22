b6fd33ac0d2c0ee31d45a309c2e119af73f22109
marisbotero <maris2143@gmail.com>
2017-12-21 20:23:07 -0500

    api with mongo

---
+++ agent/main.py
@@ -0,0 +1,29 @@
+import requests
+import pymongo
+
+API_URL = 'https://api.coinmarketcap.com/v1/ticker/'
+
+def get_db_connection(uri):
+    client = pymongo.MongoClient(uri)
+    return  client.cryptongo
+
+def get_crytocurrencies_from_api():
+    r = requests.get(API_URL)
+    if r.status_code == 200:
+        result = r.json()
+        return  result
+
+    raise Exception('Api Error')
+
+
+def check_if_exists(db_connection, ticker_data):
+    pass
+
+def save_ticket(db_connection, ticker_data = None):
+    if not ticker_data:
+        return  False
+
+    if check_if_exists(db_connection, ticker_data):
+        return  False
+
+
