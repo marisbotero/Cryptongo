c3ececbffbdc37e3518c1f2faca6f22b0acd34d7
marisbotero <maris2143@gmail.com>
2017-12-24 18:00:33 -0500

    ticket_hash

--- agent/main.py
+++ agent/main.py
@@ -15,6 +15,15 @@ def get_crytocurrencies_from_api():
 
     raise Exception('Api Error')
 
+def get_hash():
+    pass
+
+def first_element():
+    pass
+
+def get_ticker_hash():
+    pass
+
 
 def check_if_exists(db_connection, ticker_data):
     pass
