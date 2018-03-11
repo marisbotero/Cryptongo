f8570918c25c186d70cebc7b97533d5b354a25c9
marisbotero <maris2143@gmail.com>
2018-03-11 14:30:54 -0500

    Finished api main

--- api/main.py
+++ api/main.py
@@ -6,7 +6,7 @@ def get_db_connection(uri):
     return  client.cryptongo
 
 app = Flask(__name__)
-db_connection = get_db_connection('mongodb://localhost:27017')
+db_connection = get_db_connection('mongodb://localhost:27017/')
 
 def get_documents():
 	params ={}
@@ -19,7 +19,26 @@ def get_documents():
 	return list(cursor)
 
 def get_top20():
-	pass
+	params = {}
+	name = int(request.args.get('name', ''))
+	limit = int(request.args.get('limit', 0))
+	if name:
+		params.udpate({'name':name})
+        params.update({'rank': {'$lte': 20}})
+	cursor = db_connection.tickers.find(
+		params, {'_id': 0, 'ticker_hash': 0}
+        ).limit(limit)
+	return list(cursor)
+
+	
 
 def remove_currency():
-	pass
+	params = {}
+    name = request.args.get('name', '')
+    if name:
+        params.update({'name': name})
+    else:
+        return False
+    return db_connection.tickers.delete_many(
+        params
+    ).deleted_count
